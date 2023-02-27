fc7601253996751a195f5937d8e0b574a5033398
gchtfy6 <109897426+gchtfy6@users.noreply.github.com>
2023-02-27 02:00:04 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 设置各变量
 WSPATH=${WSPATH:-'argo'}
-UUID=${UUID:-'d76fdffc-4d32-4215-b794-a7680d2b5f2d'}
+UUID=${UUID:-'4a72eb34-885f-4f9d-ac89-eb7d80774f97'}
 
 generate_config() {
   cat > config.json << EOF
